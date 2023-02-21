265d72cb3223bd33801b5bd28062f53a906fd3e4
Varun Kumar <87888762+VarunkumarCS@users.noreply.github.com>
2023-02-21 17:49:47 -0500

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -16,11 +16,11 @@ class Town:
         parmateres = {'APPID': '#YOUR OWN API ID', 'q': 'Piscataway,NJ,US'}
         r = requests.get(url, params= parmateres)
 
-        #Printing the data for Piscataway, New Jersey
+        #PRinting the data for Piscataway< New Jersey
         print("The Requested data via API of Piscataway, NJ is: ")
         dataobj = r.json()
         time.sleep(1)
-        pp(json.dumps(dataobj, indent= 4))
+        print(json.dumps(dataobj, indent= 4))
         print()
         
         print("The keys of the Requested API are:")
@@ -52,6 +52,30 @@ class Town:
             print("The error line number is : ", error2.lineno)
             print("The column number is:", error2.colno)
 
+        try:
+            self.country = dataobj['sys']['country']
+        except JSONDecodeError as error3:
+            print("JSON DATA decoding error:")
+            print(error3.msg)
+            print("The error line number is : ", error3.lineno)
+            print("The column number is:", error3.colno)
+
+        try:
+            self.timezone = dataobj['timezone']
+        except JSONDecodeError as error4:
+            print("JSON DATA decoding error:")
+            print(error4.msg)
+            print("The error line number is : ", error4.lineno)
+            print("The column number is:", error4.colno)
+    
+        try: 
+            self.weather = dataobj['weather']
+        except JSONDecodeError as error5:
+            print("JSON DATA decoding error:")
+            print(error5.msg)
+            print("The error line number is : ", error5.lineno)
+            print("The column number is:", error5.colno)
+       
     def printing_all_the_values(self):
         units = 'F'
 
@@ -59,12 +83,26 @@ class Town:
         time.sleep(1)
         print()
 
-        print(f"The Temperature in the Piscataway,NJ is: {self.temp} {units}")
+        print(f"Piscataway is in which country?: {self.country}")
+        time.sleep(1)
+        print()
+
+        print(f"The timezone of Piscataway is: {self.timezone}")
+        time.sleep(1)
+        print()
+
+        print(f"The Temperature in Piscataway,NJ is: {self.temp} {units}")
         time.sleep(1)
         print()
         
-        print(f"The visibilty in the Piscataway,NJ is: {self.visibilty}")
+        print(f"The visibilty in Piscataway,NJ is: {self.visibilty}")
+        time.sleep(1)
         print()
+
+        print(f"The weather in Piscataway is: {self.weather}")
+        time.sleep(1)
+        print()
+
 
 my_town = Town("Piscataway,NJ", 40.5549, 74.4643)
 my_town.printing_all_the_values()
